87b06cbd91db970743e36f0fd7e98f7ec3ed95df
boolie205 <32076023+boolie205@users.noreply.github.com>
2018-05-01 16:41:30 +0100

    Added some console stuff to app.js
    eyy

--- Gamefiles/app.js
+++ Gamefiles/app.js
@@ -10,3 +10,8 @@ app.get('/',function(req, res) {
 app.use('/client',express.static(__dirname + '/client'));
 
 serv.listen(2000);
+
+console.log('Server started, top kek. Also robin congrats on making it work.');
+console.log('Not to mention how seriously annoying Express is tbh mate');
+console.log('Eyyy');
+
